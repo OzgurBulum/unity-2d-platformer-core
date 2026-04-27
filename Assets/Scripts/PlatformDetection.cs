22aeda5d57dbdd0c6727332598ea4e5e675c193f
Özgür Bülüm <ozgurbulum5@gmail.com>
2026-04-27 15:21:56 +0300

    Fix mobile controls - compile time platform detection

--- Assets/Scripts/PlatformDetection.cs
+++ Assets/Scripts/PlatformDetection.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class PlatformDetection : MonoBehaviour
 {
     void Start()
-{
-    // Runtime platform kontrolü (Editor'de test için)
-    bool isMobilePlatform = Application.platform == RuntimePlatform.Android || 
-                           Application.platform == RuntimePlatform.IPhonePlayer;
-    
-    gameObject.SetActive(isMobilePlatform);
-    
-    Debug.Log("Platform: " + Application.platform + " | Mobile Controls: " + isMobilePlatform);
-}
+    {
+        #if UNITY_ANDROID || UNITY_IOS
+        gameObject.SetActive(true);
+        #else
+        gameObject.SetActive(false);
+        #endif
+    }
 }
